96601b20516fb22b2fc913552cadfbcfddca12b6
kokikitamura <ktmrkok1130@gmail.com>
2024-09-13 04:44:12 +0900

    UsersControllerの認可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update, :withdraw, :destroy]
+  before_action :correct_user,   only: [:show, :edit, :update, :withdraw, :destroy]
+
   def show
     @user = User.find(params[:id])
   end
@@ -47,5 +50,22 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+    # beforeフィルタ
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(current_user, status: :see_other) unless current_user?(@user)
     end
 end
