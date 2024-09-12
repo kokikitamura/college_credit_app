07b0b0426b56f2d7466c49b12f7fdbc2447e80fa
kokikitamura <ktmrkok1130@gmail.com>
2024-09-13 04:36:57 +0900

    ログイン機能を作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
+      reset_session
+      log_in user
+      flash[:success] = "ログインしました"
+      redirect_to forwarding_url || user
+    else
+      flash.now[:danger] = "メールアドレスまたはパスワードが正しくありません。"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "ログアウトしました"
+    redirect_to login_url, status: :see_other
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,35 @@
+module SessionsHelper
+
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+      # 現在のユーザーをログアウトする
+  def log_out
+    reset_session
+    @current_user = nil   # 安全のため
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+end
