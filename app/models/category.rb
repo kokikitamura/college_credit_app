d166f3a75d02af9248e628b1e7a7e72cb9ebcc34
kokikitamura <ktmrkok1130@gmail.com>
2024-09-14 19:06:19 +0900

    Categoryモデルを作成

---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+class Category < ApplicationRecord
+  belongs_to :user
+  has_ancestry
+  default_scope -> { order(created_at: :asc) }
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :credits, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_11_124027) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_12_214647) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "credits"
+    t.string "ancestry"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -18,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_11_124027) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories", "users"
 end
