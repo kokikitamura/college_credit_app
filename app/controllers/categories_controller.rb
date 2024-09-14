a94a7d7eb4ad1a1f5019be5b48481179f898e875
kokikitamura <ktmrkok1130@gmail.com>
2024-09-15 03:37:03 +0900

    Categoryのnewを作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
     @categories = Category.where(user_id: current_user.id)
     @all_credits = 0
     @categories.each do |category|
-      @all_credits += category.credits if category.root?   #主専攻だけ足す
+      @all_credits += category.credits if category.root?   #親区分だけ足す
     end
     @all_credits #単位の総数
   end
@@ -12,9 +12,28 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    if params[:category][:ancestry].empty?
+      @category = current_user.categories.build(category_params)
+      if @category.save
+        flash[:success] = "追加しました"
+        redirect_to categories_path
+      else
+        render 'new', status: :unprocessable_entity
+      end
+    else
+      parent_category = Category.find_by(user_id: current_user.id, id: params[:category][:ancestry])
+      @category = parent_category.children.build(category_params)
+      if @category.save
+        flash[:success] = "追加しました"
+        redirect_to categories_path
+      else
+        render 'new', status: :unprocessable_entity
+      end
+    end
   end
 
   def edit
@@ -25,4 +44,10 @@ class CategoriesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name, :credits).merge(user_id: current_user.id)
+    end
 end
